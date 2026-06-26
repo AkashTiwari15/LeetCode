b4f885780207d4756b6976e1bb49000fd9e23e14
AkashTiwari15 <work.tiwariakash@gmail.com>
2026-06-26 12:23:05 +0530

    Time: 15 ms (6.13%), Space: 47.4 MB (79.94%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -4,27 +4,54 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        boolean[] row = new boolean[n];
-        boolean[] col = new boolean[m];
+        // boolean[] row = new boolean[n];
+        // boolean[] col = new boolean[m];
 
-        // Mark rows and columns with zero
+        // // Mark rows and columns with zero
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+
+        //         if (matrix[i][j] == 0) {
+        //             row[i] = true;
+        //             col[j] = true;
+        //         }
+        //     }
+        // }
+
+        // // Set zeros
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < m; j++) {
+
+        //         if (row[i] || col[j]) {
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
+
+        // SECOND APPROACH 
+        
+        int[][] res = new int[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
+                res[i][j] = matrix[i][j];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == 0) {
-                    row[i] = true;
-                    col[j] = true;
+
+                    for (int k = 0; k < n; k++) {
+                        res[k][j] = 0;
+                    }
+                    for (int k = 0; k < m; k++) {
+                        res[i][k] = 0;
+                    }
                 }
             }
         }
-
-        // Set zeros
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-
-                if (row[i] || col[j]) {
-                    matrix[i][j] = 0;
-                }
+                matrix[i][j] = res[i][j];
             }
         }
     }
