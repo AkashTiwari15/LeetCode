90fba0bd584bb4cee1ffb1d12ffcccb51ac28f79
AkashTiwari15 <work.tiwariakash@gmail.com>
2026-07-09 13:57:53 +0530

    Time: 1 ms (75.92%), Space: 43.1 MB (33.21%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String interpret(String command) {
+        int n = command.length();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i<n ; i++){
+            if(command.charAt(i)=='G') sb.append('G');
+            if(command.charAt(i)=='('){
+                if(command.charAt(i+1)==')'){
+                    sb.append('o'); i++;
+                }else{
+                    sb.append("al");
+                    i = i+3;
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
